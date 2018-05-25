5f03a95f605031e541cf807f4652a46b3eb10f29
AnusreeP <39556502+AnusreeP@users.noreply.github.com>
2018-05-25 09:14:34 +0530

    Add files via upload

---
+++ multiple.java
@@ -0,0 +1,21 @@
+
+public class multiple {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num;
+		System.out.println("Multiple of 5 in the range 0 to 50");
+		for(num=5;num<=50;num++)
+		{
+			if(num%5 ==0)
+			{
+				System.out.println(num);
+			}
+			else
+			{
+				
+			}
+		}
+	}
+
+}
