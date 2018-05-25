26d9803e3191c3062c329e8c9d4a37a4b4d6b66b
AnusreeP <39556502+AnusreeP@users.noreply.github.com>
2018-05-25 09:11:39 +0530

    Add files via upload

---
+++ DateTime.java
@@ -0,0 +1,17 @@
+import java.time.LocalDateTime;
+
+public class DateTime {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		LocalDateTime current = LocalDateTime.now();
+
+        System.out.println("Current Date and Time is: " + current);
+
+
+	}
+
+}
+
+
