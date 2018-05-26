b4ac0c645e4868d1503d3d715c668c1daa09291d
AnusreeP <39556502+AnusreeP@users.noreply.github.com>
2018-05-26 09:32:08 +0530

    Add files via upload

---
+++ alphabets.java
@@ -0,0 +1,16 @@
+public class alphabets {
+	public static void main(String[] args) {
+			char alphabets;
+		System.out.println("Uppercase Alphabets is :");
+		for(alphabets='A';alphabets<='Z';alphabets++)
+		{
+			System.out.println(alphabets);
+		}
+		System.out.println("Lowercase Alphabets is :");
+		for(alphabets='a';alphabets<='z';alphabets++)
+		{
+			System.out.println(alphabets);
+		}
+	}
+
+}
