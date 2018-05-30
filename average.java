cbcc554d37ade505bf0a33d0f07156e150bd2622
AnusreeP <39556502+AnusreeP@users.noreply.github.com>
2018-05-30 13:36:27 +0530

    Add files via upload

---
+++ average.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class average {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			
+		Scanner in = new Scanner(System.in);
+		   
+		  System.out.print("Enter first number: ");
+		  int num1 = in.nextInt();
+		   
+		  System.out.print("Enter second number: ");
+		  int num2 = in.nextInt();
+		   
+		  System.out.print("Enter third number: ");
+		  int num3 = in.nextInt();  
+		  System.out.println("Average number is: " +  (num1 + num2 + num3 ) / 3);
+		
+	}
+
+}
